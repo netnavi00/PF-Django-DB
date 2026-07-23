ab1ab37987b2bdad28b6f741c747cfe89c10de28
JEVN <netnavi00@gmail.com>
2026-07-23 17:02:53 -0600

    Se modifica models.py para ordenar croonologicamente y arreglar issue con varaibles en plural

--- Avance_4/mis_videos/streaming/models.py
+++ Avance_4/mis_videos/streaming/models.py
@@ -6,7 +6,12 @@ class TBL_Usuario(models.Model):
     # Campo llave, alfanumérico de 10 caracteres
     id_nomina = models.CharField(max_length=10, primary_key=True, db_column='id_nomina') 
     # Nombre alfanumérico de 50 caracteres
-    nombre = models.CharField(max_length=50) 
+    nombre = models.CharField(max_length=50)
+
+    class Meta:
+        ordering = ['-id_nomina']
+        verbose_name = 'TBL_Usuario'     
+        verbose_name_plural = 'TBL_Usuarios' 
 
     def __str__(self):
         return f"{self.id_nomina} - {self.nombre}"
@@ -26,15 +31,27 @@ class TBL_Video(models.Model):
         upload_to='videos/',
         validators=[FileExtensionValidator(allowed_extensions=['mp4', 'avi', 'mov', 'mkv', 'webm'])] # <- Restricción
     )
+    class Meta:
+        ordering = ['-id_video']
+        verbose_name = 'TBL_Video'     
+        verbose_name_plural = 'TBL_Videos'
+
     def __str__(self):
         return self.nombre
 
 # c. Tabla para guardar los videos que subirá el usuario (relación intermedia)
 class TBL_Usuario_Video(models.Model):
-    # Id del usuario vinculado como llave foránea a TBL_Usuario
+    # Id del usuario vinculado ,como llave foránea a TBL_Usuario
     id_usuario = models.ForeignKey(TBL_Usuario, on_delete=models.CASCADE, db_column='id_usuario') 
     # Id del video vinculado como llave foránea a TBL_Video
     id_video = models.ForeignKey(TBL_Video, on_delete=models.CASCADE, db_column='id_video') 
 
+    class Meta:
+        ordering = ['-id']  # Si esta tabla usa el ID por defecto de Django, esto la ordena también de reciente a antiguo
+        verbose_name = 'TBL_Usuario_Video'     
+        verbose_name_plural = 'TBL_Usuarios_Videos'
+
     def __str__(self):
-        return f"Usuario: {self.id_usuario.id_nomina} -> Video ID: {self.id_video.id_video}"
+        # Si esta es la tabla que ves en el Admin y solo quieres ver el nombre del video:
+        return self.id_video.nombre
+    
